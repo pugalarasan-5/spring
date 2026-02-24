3bc7e9a63fd0c7a5ab680453a3dffe997be50242
pugalarasan-5 <pugalarasan302@gmail.com>
2026-02-24 11:52:13 +0530

    init'

---
+++ digital-payment-management/src/main/java/com/example/digital_payment_management/DigitalPaymentManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.digital_payment_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalPaymentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalPaymentManagementApplication.class, args);
+		System.out.println("Digital Payment Management Application started successfully!");
+	}
+
+}
